13e7a5aae63175324ba17e2d25aae842c8dac6e7
Amanda Lourenco <amandalourenco67@gmail.com>
2020-07-06 20:41:16 -0300

    primeiro commit

---
+++ src/test/java/br/com/erudio/restwithspring/RestWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
